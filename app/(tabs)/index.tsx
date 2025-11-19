6fea4e01b8926794d6d52718930b206dd5816e2c
kwonjin2 <your_github_email>
2025-11-19 14:11:30 +0900

    fix(app): 로또 번호 초간단 생성, 특별 생성 각각에 맞게 생성되도록  코드 수정

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,6 +2,7 @@ import { View, Text, StyleSheet, Modal } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useState, useEffect } from 'react';
 import generateLottoNumbers from '@/src/utils/generateLotto';
+import generateSpecialLotto from '@/src/utils/generateSpecialLotto';
 import LottoCard from '@/src/components/LottoCard';
 import LottoModal from '@/src/components/LottoModal';
 
@@ -15,6 +16,7 @@ const STORAGE_KEY = '@lotto_purchase_history';
 
 export default function CreateLotto() {
   const [generatedNumbers, setGeneratedNumbers] = useState<number[]>([]);
+  const [specialNumbers, setSpecialNumbers] = useState<number[]>([]);
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [purchaseHistory, setPurchaseHistory] = useState<LottoEntry[]>([]);
 
@@ -47,6 +49,7 @@ export default function CreateLotto() {
   const handleSimpleGenerate = () => {
     const newNumbers = generateLottoNumbers();
     setGeneratedNumbers(newNumbers);
+    setSpecialNumbers([]);
   };
 
   const handleSpecialGenerate = () => {
@@ -54,16 +57,20 @@ export default function CreateLotto() {
   };
 
   const handleModalGenerate = (userInfo: { name: string; date: string }) => {
-    console.log('특별 로또 생성 정보:', userInfo);
-    const specialNumbers = generateLottoNumbers();
-    setGeneratedNumbers(specialNumbers);
+    const newSpecialNumbers = generateSpecialLotto(userInfo);
+
+    setGeneratedNumbers([]);
+    setSpecialNumbers(newSpecialNumbers);
     setIsModalVisible(false);
   };
 
   const handlePurchase = () => {
+    const numbersToPurchase =
+      specialNumbers.length > 0 ? specialNumbers : generatedNumbers;
+
     const newEntry: LottoEntry = {
       id: Date.now(),
-      numbers: [...generatedNumbers],
+      numbers: [...numbersToPurchase],
       date: new Date().toLocaleString('ko-KR', {
         year: 'numeric',
         month: '2-digit',
@@ -75,7 +82,9 @@ export default function CreateLotto() {
 
     alert('해당 로또 번호를 구매했습니다.\n 기록 페이지에서 확인해 보세요.');
     setPurchaseHistory((prev) => [...prev, newEntry]);
+
     setGeneratedNumbers([]);
+    setSpecialNumbers([]);
   };
 
   return (
@@ -91,13 +100,14 @@ export default function CreateLotto() {
         buttonColor="#FFD700"
         buttonTextColor="#25292e"
       />
+
       <LottoCard
         header="✨ 특별 로또"
         description={[
           '나만의 특별한 숫자를 기반으로',
           '맞춤형 로또를 생성해보세요',
         ]}
-        generatedNumbers={[]}
+        generatedNumbers={specialNumbers}
         onGenerate={handleSpecialGenerate}
         purchaseLotto={handlePurchase}
         buttonTitle="특별 생성하기"
