ca8c69658cbed675be07a1b4c84453937aea2233
kwonjin2 <your_github_email>
2025-11-19 11:47:46 +0900

    feat(app): 간단 로또 생성 기능 구현

--- app/(tabs)/create.tsx
+++ app/(tabs)/create.tsx
@@ -1,31 +1,44 @@
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { useState } from 'react';
-import { useRouter } from 'expo-router';
 import generateLottoNumbers from '@/src/utils/generateLotto';
 import LottoBall from '@/src/components/LottoBall';
 
 export default function CreateLotto() {
   const [generatedNumbers, setGeneratedNumbers] = useState<number[]>([]);
-  const router = useRouter();
 
   const handleGenerate = () => {
     const newNumbers = generateLottoNumbers();
     setGeneratedNumbers(newNumbers);
   };
 
-  const goBack = () => {
-    router.back();
-  };
-
   return (
     <View style={styles.container}>
-      <Button title="뒤로 가기" onPress={goBack} />
-      <Text style={styles.text}>로또 번호 만들기</Text>
-      <Button title="생성하기" onPress={handleGenerate} />
-      <View style={styles.lottos}>
-        {generatedNumbers.map((number, index) => (
-          <LottoBall key={index} number={number} />
-        ))}
+      <Text style={styles.header}>LUCKY COMPASS</Text>
+      <View style={styles.simpleLotto}>
+        <Text style={styles.simpleHeader}>⚡️초간단 로또</Text>
+        <Text style={styles.simpleText}>클릭 한 번으로 랜덤 번호를</Text>
+        <Text style={styles.simpleText}>즉시 생성해드려요</Text>
+        <View style={styles.lottoContainer}>
+          <View style={styles.generateLotto}>
+            {generatedNumbers.length === 0 ? (
+              <Text style={styles.emptyText}>
+                생성된 번호가 여기에 표시됩니다.
+              </Text>
+            ) : (
+              <View style={styles.lottos}>
+                {generatedNumbers.map((number, index) => (
+                  <LottoBall key={index} number={number} />
+                ))}
+              </View>
+            )}
+          </View>
+        </View>
+        <TouchableOpacity
+          style={styles.generateButton}
+          onPress={handleGenerate}
+        >
+          <Text style={styles.generateButtonText}>번호 생성하기</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -37,16 +50,67 @@ const styles = StyleSheet.create({
     backgroundColor: '#25292e',
     paddingTop: 50,
     color: '#fff',
+    gap: 40,
+    alignItems: 'center',
+    marginTop: 21,
   },
-  button: {
-    flexDirection: 'row',
-  },
-  text: {
-    color: '#fff',
+  header: {
     fontSize: 30,
+    fontWeight: 'bold',
+    color: '#FFD700',
+  },
+  simpleLotto: {
+    width: 360,
+    height: 300,
+    backgroundColor: '#34383D',
+    borderRadius: 20,
+  },
+  simpleHeader: {
+    marginLeft: 16,
+    color: '#fff',
+    fontWeight: 'bold',
+    marginTop: 20,
+    marginBottom: 20,
+    fontSize: 28,
+  },
+  simpleText: {
+    marginLeft: 20,
+    color: '#B0B0B0',
+    fontSize: 18,
+  },
+  lottoContainer: {
+    alignItems: 'center',
+    marginTop: 17,
+  },
+  generateLotto: {
+    width: 330,
+    height: 80,
+    borderRadius: 20,
+    backgroundColor: '#2A2D32',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  emptyText: {
+    color: '#6A6A6A',
   },
   lottos: {
     flexDirection: 'row',
-    gap: 10,
+    gap: 6,
+  },
+  generateButton: {
+    position: 'absolute',
+    bottom: 20,
+    left: 15,
+    right: 15,
+    height: 45,
+    backgroundColor: '#FFD700',
+    borderRadius: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  generateButtonText: {
+    color: '#25292e',
+    fontSize: 20,
+    fontWeight: 'bold',
   },
 });
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,34 +3,147 @@ import { View, Text, StyleSheet } from 'react-native';
 export default function HomePage() {
   return (
     <View style={styles.container}>
-      <Text style={styles.titleText}>Lotto 살래?</Text>
-      <View>
-        <Text style={styles.text}>간단하게 랜덤 로또를 받아보세요</Text>
+      <View style={styles.circularLayout}>
+        <Text style={styles.titleText}>놋또</Text>
+        <View
+          style={[styles.lotto, styles.ball1, { backgroundColor: '#FF6B6B' }]}
+        >
+          <Text style={styles.lottoNumber}>26</Text>
+        </View>
+        <View
+          style={[styles.lotto, styles.ball2, { backgroundColor: '#4CAF50' }]}
+        >
+          <Text style={styles.lottoNumber}>33</Text>
+        </View>
+        <View
+          style={[styles.lotto, styles.ball3, { backgroundColor: '#FFD700' }]}
+        >
+          <Text style={styles.lottoNumber}>39</Text>
+        </View>
+        <View
+          style={[styles.lotto, styles.ball4, { backgroundColor: '#2196F3' }]}
+        >
+          <Text style={styles.lottoNumber}>41</Text>
+        </View>
+        <View
+          style={[styles.lotto, styles.ball5, { backgroundColor: '#9C27B0' }]}
+        >
+          <Text style={styles.lottoNumber}>38</Text>
+        </View>
+        <View
+          style={[styles.lotto, styles.ball6, { backgroundColor: '#FF5722' }]}
+        >
+          <Text style={styles.lottoNumber}>30</Text>
+        </View>
       </View>
+
       <View>
-        <Text style={styles.text}>
-          나만의 특별한 숫자를 기반으로 로또를 받아보세요
-        </Text>
-      </View>
-      <View>
-        <Text style={styles.text}>생성한 로또 번호를 한 눈에 보세요</Text>
+        <Text>대시보드</Text>
+        <View style={{ flexDirection: 'row', gap: 20 }}>
+          {/* <Text>대시보드</Text> */}
+          <View
+            style={{
+              backgroundColor: '#34383D',
+              width: 160,
+              height: 100,
+              borderRadius: 20,
+            }}
+          >
+            <Text>당첨률</Text>
+            <Text>0%</Text>
+            <Text>0 / 0 당첨</Text>
+          </View>
+          <View
+            style={{
+              backgroundColor: '#34383D',
+              width: 160,
+              height: 100,
+              borderRadius: 20,
+            }}
+          >
+            <Text>구매횟수</Text>
+            <Text>0회</Text>
+            <Text>총 0원 투자</Text>
+          </View>
+          <View></View>
+        </View>
       </View>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#25292e',
     alignItems: 'center',
-    paddingTop: 100,
+    paddingTop: 40,
+  },
+  circularLayout: {
+    width: 250,
+    height: 250,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'relative',
+    marginBottom: 40,
+  },
+  lotto: {
+    width: 45,
+    height: 45,
+    borderRadius: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  lottoNumber: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 18,
   },
   titleText: {
-    color: '#fff',
+    color: '#FFD700',
+    fontWeight: 'bold',
     fontSize: 30,
+    zIndex: 1,
   },
-  text: {
-    color: '#fff',
+  ball1: {
+    backgroundColor: '#FF6B6B',
+    position: 'absolute',
+    top: 0,
+    left: '50%',
+    transform: [{ translateX: -22.5 }, { translateY: 22.5 }],
+  },
+  ball2: {
+    backgroundColor: '#4CAF50',
+    position: 'absolute',
+    top: 40,
+    right: 0,
+    transform: [{ translateX: -30 }, { translateY: 25 }],
+  },
+  ball3: {
+    backgroundColor: '#bca10bff',
+    position: 'absolute',
+    bottom: 40,
+    right: 0,
+    transform: [{ translateX: -30 }, { translateY: -25 }],
+  },
+  ball4: {
+    backgroundColor: '#2196F3',
+    position: 'absolute',
+    bottom: 0,
+    left: '50%',
+    transform: [{ translateX: -22.5 }, { translateY: -25 }],
+  },
+  ball5: {
+    backgroundColor: '#9C27B0',
+    position: 'absolute',
+    bottom: 40,
+    left: 0,
+    transform: [{ translateX: 30 }, { translateY: -25 }],
+  },
+  ball6: {
+    backgroundColor: '#FF5722',
+    position: 'absolute',
+    top: 40,
+    left: 0,
+    transform: [{ translateX: 30 }, { translateY: 25 }],
   },
 });
